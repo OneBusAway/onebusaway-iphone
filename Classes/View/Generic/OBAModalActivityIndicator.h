a5bc42f7f5c50b4ce7fc5ea9cfc4aa724178add9
Brian Ferris <bdferris@google.com>
2011-06-29 01:51:01 +0000

    Remove duplicate classes from onebusaway-iphone that already appeared in onebusaway-iphone-common

--- Classes/View/Generic/OBAModalActivityIndicator.h
+++
@@ -1,9 +0,0 @@
-@interface OBAModalActivityIndicator : NSObject {
-	UIView * _modalView;
-	UIActivityIndicatorView * _activityIndicatorView;
-}
-
-- (void) show:(UIView*)view;
-- (void) hide;
-
-@end
--- Classes/View/Generic/OBAWebViewController.h
+++
@@ -1,7 +0,0 @@
-@interface OBAWebViewController : UIViewController <UIWebViewDelegate> {
-
-}
-
-+(OBAWebViewController*)pushOntoViewController:(UIViewController*)parent withHtml:(NSString*)html withTitle:(NSString*)title;
-
-@end
