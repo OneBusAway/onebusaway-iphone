c92c7fe88200f79deff048c82efc853b467c9c16
Aaron Brethorst <aaron@brethorsting.com>
2012-09-15 14:24:53 -0700

    Tweak display params.

--- view_controllers/OBASearchResultsMapViewController.m
+++ view_controllers/OBASearchResultsMapViewController.m
@@ -155,9 +155,9 @@ static const double kStopsInRegionRefreshDelayOnLocate = 0.1;
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"List", @"Right bar button item in map") style:UIBarButtonItemStyleBordered target:self action:@selector(onListButton:)];
     
     self.searchResultsListViewController = [[OBASearchResultsListViewController alloc] initWithContext:_appContext searchControllerResult:nil];
-    self.searchResultsListViewController.view.frame = CGRectMake(0, 0, 150, CGRectGetHeight(self.view.bounds));
+    self.searchResultsListViewController.view.frame = CGRectMake(0, 0, 250, CGRectGetHeight(self.view.bounds));
     self.paperFoldView = [[PaperFoldView alloc] initWithFrame:self.view.bounds];
-    [self.paperFoldView setRightFoldContentView:self.searchResultsListViewController.view rightViewFoldCount:4 rightViewPullFactor:0.9];
+    [self.paperFoldView setRightFoldContentView:self.searchResultsListViewController.view rightViewFoldCount:4 rightViewPullFactor:0.5];
     self.paperFoldView.enableRightFoldDragging = NO;
 
     UIView *originalView = self.view;
