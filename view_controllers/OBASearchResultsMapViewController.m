20958a066834b85610b83203e42852da6cd33855
Ben Bodenmiller <bbodenmiller@hotmail.com>
2013-07-29 19:36:15 -0700

    move out min zoom level

--- view_controllers/OBASearchResultsMapViewController.m
+++ view_controllers/OBASearchResultsMapViewController.m
@@ -43,14 +43,14 @@
 
 // Radius in meters
 static const double kDefaultMapRadius = 100;
-static const double kMinMapRadius = 150;
+static const double kMinMapRadius = 210;
 static const double kMaxLatDeltaToShowStops = 0.008;
 static const double kRegionScaleFactor = 1.5;
 static const double kMinRegionDeltaToDetectUserDrag = 50;
 
 static const double kRegionChangeRequestsTimeToLive = 3.0;
 
-static const double kMaxMapDistanceFromCurrentLocationForNearby = 800;
+static const double kMaxMapDistanceFromCurrentLocationForNearby = 850;
 static const double kPaddingScaleFactor = 1.075;
 static const NSUInteger kShowNClosestStops = 4;
 
