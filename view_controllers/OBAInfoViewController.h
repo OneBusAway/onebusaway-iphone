418c1ebfab9ff2124843e5f6b9ca3ab9b11aefde
Ben Bodenmiller <bbodenmiller@hotmail.com>
2013-09-27 13:24:34 -0700

    fix contact us, agencies, and settings buttons

--- view_controllers/OBAInfoViewController.h
+++ view_controllers/OBAInfoViewController.h
@@ -9,6 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface OBAInfoViewController : UIViewController<UITableViewDataSource, UITableViewDelegate>
+
+- (void)openContactUs;
+- (void)openSettings;
+- (void)openAgencies;
+
 @property(nonatomic,strong) IBOutlet UIView *headerView;
 @property(nonatomic,strong) IBOutlet UITableView *tableView;
 @end
