38020771f2f208e90e669140421e1d7d98da024b
Jon Bell <jonathan_bell@icloud.com>
2013-11-04 17:06:19 -0800

    Created aggregate target ‘Versioning’ to to execute shell scripts to set bundle identifier and bundle version via Infoplist preprocessor file. Added ‘Versioning’ target to the dependencies list of ‘OneBusAway’ target. Enabled Info.plist preprocessing and set the Info.plist prefix header file in ‘OneBusAway’ target build settings. Modified setBuildVersion script define GIT_COMMIT_SHA in infoplist.prefix. Modified setBundleIdentifierForAppStore script to define BUNDLE_IDENTIFIER in infoplist.prefix. Set CFBundleIdentifier to BUNDLE_IDENTIFIER. Set CFBundleVersion to GIT_COMMIT_SHA. Added build run script phase to ‘OneBusAway’ target to remove Infoplist preprocessor file from project temp directory. Added scripts folder to the project files under the Resources group and excluded the files from all targets so they’re not copied to the app bundle.

--- scripts/setBuildVersion.sh
+++ scripts/setBuildVersion.sh
@@ -8,4 +8,4 @@ then
 gitCommitSHA+="+"
 fi
 # Set bundle build version to current git commit short SHA with dirty indicator
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $gitCommitSHA" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
+echo "#define GIT_COMMIT_SHA $gitCommitSHA" >> $PROJECT_TEMP_DIR/infoplist.prefix
