750c73da03385aec741e44630a34a3d7daafa567
Jon Bell <jonathan_bell@icloud.com>
2013-11-18 14:36:50 -0800

    Restored setBuildVersion.sh script and modified it to set CFBundleVersion to the git commit SHA for only Debug builds.

---
+++ scripts/setBuildVersion.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ "$CONFIGURATION" == "Debug" ]
+then
+    # Get current git commit short SHA
+    gitCommitSHA=$(git rev-parse --short HEAD)
+    # Check if the git working copy is dirty
+    if test -n "$(git status --porcelain)"
+    then
+        # Append a plus character to the git commit short SHA to indicate dirty changes in build
+        gitCommitSHA+="+"
+    fi
+    # Set bundle build version to current git commit short SHA with dirty indicator
+    echo "#define GIT_COMMIT_SHA $gitCommitSHA" >> $PROJECT_TEMP_DIR/infoplist.prefix
+fi
+# Tell Xcode to preprocess Info.plist
+touch $PROJECT_DIR/Info.plist
