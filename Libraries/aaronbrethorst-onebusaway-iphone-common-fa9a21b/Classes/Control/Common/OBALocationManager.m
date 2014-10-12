9afc6741c24a52c76669a87f6d455a88c0b0dd30
Aaron Brethorst <aaron@brethorsting.com>
2014-10-12 15:24:44 -0700

    Fix for building against iOS 7 SDK

--- Libraries/aaronbrethorst-onebusaway-iphone-common-fa9a21b/Classes/Control/Common/OBALocationManager.m
+++ Libraries/aaronbrethorst-onebusaway-iphone-common-fa9a21b/Classes/Control/Common/OBALocationManager.m
@@ -87,7 +87,7 @@ static const NSTimeInterval kSuccessiveLocationComparisonWindow = 3;
 
 - (void)requestInUseAuthorization {
     if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"8.0")) {
-        [_locationManager requestWhenInUseAuthorization];
+        [_locationManager performSelector:@selector(requestWhenInUseAuthorization) withObject:nil];
     }
 }
 
