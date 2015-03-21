a2a8d58294534a9f92fb387b4edbe6f66e0ca117
Vania Kurniawati <vania.baek@gmail.com>
2015-03-21 15:59:49 -0700

    added OBAReport.h (data model for report/issues) and added OBASubmitReportViewController (view controller where user will add a new report of bus being full or not)

---
+++ models/unmanaged/OBAReport.m
@@ -0,0 +1,13 @@
+//
+//  OBAReport.m
+//  org.onebusaway.iphone
+//
+//  Created by Vania Kurniawati on 3/21/15.
+//  Copyright (c) 2015 OneBusAway. All rights reserved.
+//
+
+#import "OBAReport.h"
+
+@implementation OBAReport
+
+@end
