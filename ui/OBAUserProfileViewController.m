97392c2756cdaf20064070ed0df827a6a1a152ce
Vania <vania.baek@gmail.com>
2015-03-22 10:59:17 -0700

    changes to the user profile view controller UI and content

--- ui/OBAUserProfileViewController.m
+++ ui/OBAUserProfileViewController.m
@@ -8,7 +8,11 @@
 
 #import "OBAUserProfileViewController.h"
 
-@interface OBAUserProfileViewController ()
+@interface OBAUserProfileViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *userPicture;
+@property (weak, nonatomic) IBOutlet UILabel *userName;
+@property (weak, nonatomic) IBOutlet UILabel *userPoints;
+@property (weak, nonatomic) IBOutlet UIView *profileBox;
 
 @end
 
@@ -16,7 +20,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+  
+  self.profileBox.layer.cornerRadius = 20;
+  
+  //User picture set-up
+  self.userPicture.layer.cornerRadius = 50;
+  self.userPicture.layer.masksToBounds = true;
+  self.userPicture.layer.borderColor = [[UIColor whiteColor] CGColor];
+  self.userPicture.layer.borderWidth = 4;
+  self.userPicture.contentMode = UIViewContentModeScaleAspectFill;
+  self.userPicture.image = [UIImage imageNamed:@"juju.jpg"];
+  
+  self.userName.text = @"Jack Spade"; //replace with username
+  
+  //Camera button
+  UIBarButtonItem *cameraButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCamera target:self action:@selector(cameraButtonPressed)];
+  self.navigationItem.rightBarButtonItem = cameraButton;
 }
+
+//Camera Button Pressed
+-(void)cameraButtonPressed {
+  UIImagePickerController *picturePicker = [[UIImagePickerController alloc] init];
+  picturePicker.delegate = self;
+  picturePicker.allowsEditing = true;
+  picturePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+  
+  [self.navigationController presentViewController:picturePicker animated:true completion:nil];
+  
+}
+
+
+//MARK: Image Picker Controller Delegate methods
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+  
+  UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
+  self.userPicture.image = chosenImage;
+  
+  //Save selected image locally
+  NSString *documentsDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+  
+  NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"MyPicture.jpg"];
+  
+  NSData *imageData = UIImageJPEGRepresentation(chosenImage, 0.85);
+  [imageData writeToFile:filePath atomically:YES];
+  
+  [picker dismissViewControllerAnimated:YES completion:NULL];
+  
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+  [picker dismissViewControllerAnimated:YES completion:NULL];
+  
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
