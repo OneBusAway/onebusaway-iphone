7c96b1e71ab0c9b692ac8c9d66a8d008f29d5a01
Tom Lai <tomtclai@gmail.com>
2015-08-21 17:22:39 -0700

    Fix issue where table header text overlaps with cell text

--- sdk/sdk/Helpers/OBACommonV1.h
+++ sdk/sdk/Helpers/OBACommonV1.h
@@ -27,7 +27,7 @@ typedef NS_ENUM(NSInteger, OBASearchViewType) {
 #define OBARGBACOLOR(__r, __g, __b, __a) [UIColor colorWithRed:(__r / 255.f) green:(__g / 255.f) blue:(__b / 255.f) alpha:__a]
 #define OBAGREENWITHALPHA(__a) [UIColor colorWithHue:(86.f/360.f) saturation:0.68f brightness:0.67f alpha:__a]
 #define OBAGREEN [UIColor colorWithHue:(86.f/360.f) saturation:0.68f brightness:0.67f alpha:1.f]
-#define OBAGREENBACKGROUND [UIColor colorWithHue:(86.f/360.f) saturation:0.68f brightness:0.67f alpha:0.1f]
+#define OBAGREENBACKGROUND [UIColor colorWithRed:0.92f green:0.95f blue:0.88f alpha:.67f]
 #define OBADARKGREEN [UIColor colorWithRed:0.2f green:.4f blue:0.f alpha:1.f]
 #define OBALABELGREEN [UIColor colorWithRed:0.f green:0.478f blue:0.f alpha:1.f]
 
