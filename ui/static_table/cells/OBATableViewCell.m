61bac8bde13f7ee8c08facdfdd989234abf57ecb
Aaron Brethorst <aaron@brethorsting.com>
2016-09-04 10:59:44 -0700

    Make long stop names wrap properly on the Arrival and Departure view
    Fixes #691 - Arrival and Departure UI needs polishing
    
    It turns out that there are bugs with handling long text in a table cell with the Value1 style. By using our own custom cell and wrapping replacement labels in that cell with a stack view, we can properly handle displaying long strings.

--- ui/static_table/cells/OBATableViewCell.m
+++ ui/static_table/cells/OBATableViewCell.m
@@ -45,13 +45,6 @@
 }
 @end
 
-@implementation OBATableViewCellValue1
-- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
-    self = [super initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:reuseIdentifier];
-    return self;
-}
-@end
-
 @implementation OBATableViewCellValue2
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
     self = [super initWithStyle:UITableViewCellStyleValue2 reuseIdentifier:reuseIdentifier];
