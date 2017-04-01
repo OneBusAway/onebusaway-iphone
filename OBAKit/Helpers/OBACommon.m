6ad30a7bafdfafd33aa2af5d3e77feca14587d15
Aaron Brethorst <aaron@brethorsting.com>
2017-04-01 00:02:54 -0700

    Only show experimental regions on the Region List controller when debug mode is enabled

--- OBAKit/Helpers/OBACommon.m
+++ OBAKit/Helpers/OBACommon.m
@@ -79,6 +79,14 @@ NSString * OBAStringFromBool(BOOL yn) {
     return obaCommonRunningInsideTests;
 }
 
++ (BOOL)debugMode {
+    return [[NSUserDefaults standardUserDefaults] boolForKey:OBADebugModeUserDefaultsKey];
+}
+
++ (void)setDebugMode:(BOOL)debugMode {
+    [[NSUserDefaults standardUserDefaults] setBool:debugMode forKey:OBADebugModeUserDefaultsKey];
+}
+
 @end
 
 
