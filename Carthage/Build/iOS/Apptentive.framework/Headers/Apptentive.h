7c7d263dab0dad12dfe2157e95093a79762eacb6
Aaron Brethorst <aaron@brethorsting.com>
2017-04-16 13:30:15 -1000

    Updates Apptentive SDK to v3.5.0
    This release includes support for iOS 10.3’s new in-app rating dialog. Yay!

--- Carthage/Build/iOS/Apptentive.framework/Headers/Apptentive.h
+++ Carthage/Build/iOS/Apptentive.framework/Headers/Apptentive.h
@@ -9,7 +9,7 @@
 
 #import <UIKit/UIKit.h>
 
-#define kApptentiveVersionString @"3.4.3"
+#define kApptentiveVersionString @"3.5.0"
 #define kApptentivePlatformString @"iOS"
 
 #ifdef __swift_compiler_version_at_least
