01e5d8bacc200293b4fa4c22c1bcf85be798dae6
Aaron Brethorst <aaron@brethorsting.com>
2019-04-02 08:46:20 -0700

    Removes all Carthage artifacts
    This is in preparation to start saving the sources rather than the built artifacts in order to reduce source churn. Ugh.

--- Carthage/Build/iOS/SMFloatingLabelTextField.framework/Headers/SMFloatingLabelTextField-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "SMFloatingLabelTextField.h"
-
-FOUNDATION_EXPORT double SMFloatingLabelTextFieldVersionNumber;
-FOUNDATION_EXPORT const unsigned char SMFloatingLabelTextFieldVersionString[];
-
