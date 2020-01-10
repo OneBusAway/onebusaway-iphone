9fea0676314fa4970b176f78d2fc210a2e4fa800
ApolloZhu <public-apollonian@outlook.com>
2020-01-10 09:11:06 -0800

    Fix encoding URL comparision tests
    Since the order in which query items appear in a URL does not matter,
    this introduces a more robust way to compare URL to its expected value,
    addressing issues caused by the unpredictable order of Swift Dictionary items.

--- OneBusAway/OneBusAwayTests/OBAKitTests/services/OBAModelService_Tests.m
+++ OneBusAway/OneBusAwayTests/OBAKitTests/services/OBAModelService_Tests.m
@@ -6,7 +6,7 @@
 //  Copyright © 2016 OneBusAway. All rights reserved.
 //
 
-#import <XCTest/XCTest.h>
+@import XCTest;
 #import "OBATestHelpers.h"
 
 @interface OBAModelService_Tests : XCTestCase
@@ -24,35 +24,63 @@
 - (void)testEncodingOfStopIDsWithAlphanumerics {
     NSURL *URL = [self URLForRequestWithStopID:@"1234" minutesBefore:5 minutesAfter:30];
     NSString *goodURLString = [NSString stringWithFormat:@"http://api.tampa.onebusaway.org/api/api/where/arrivals-and-departures-for-stop/1234.json?key=org.onebusaway.iphone&app_uid=test&app_ver=%@&version=2&minutesBefore=5&minutesAfter=30", self.appVersion];
-    XCTAssertEqualObjects(URL.absoluteString, goodURLString);
+    [self assertURL:URL equivalentToString:goodURLString];
 }
 
 - (void)testEncodingOfStopIDsWithSlashes {
     NSURL *URL = [self URLForRequestWithStopID:@"Foo/Bar" minutesBefore:5 minutesAfter:30];
-    NSString *goodURLString = [NSString stringWithFormat:@"http://api.tampa.onebusaway.org/api/api/where/arrivals-and-departures-for-stop/Foo%%2FBar.json?key=org.onebusaway.iphone&app_uid=test&app_ver=%@&version=2&minutesBefore=5&minutesAfter=30",self.appVersion];
-    XCTAssertEqualObjects(goodURLString, URL.absoluteString);
+    NSString *goodURLString = [NSString stringWithFormat:@"http://api.tampa.onebusaway.org/api/api/where/arrivals-and-departures-for-stop/Foo%%2FBar.json?key=org.onebusaway.iphone&app_uid=test&app_ver=%@&version=2&minutesBefore=5&minutesAfter=30", self.appVersion];
+    [self assertURL:URL equivalentToString:goodURLString];
 }
 
 - (void)testEncodingOfStopIDsWithSpaces {
     NSURL *URL = [self URLForRequestWithStopID:@"Foo Bar" minutesBefore:5 minutesAfter:30];
     NSString *goodURLString = [NSString stringWithFormat:@"http://api.tampa.onebusaway.org/api/api/where/arrivals-and-departures-for-stop/Foo%%20Bar.json?key=org.onebusaway.iphone&app_uid=test&app_ver=%@&version=2&minutesBefore=5&minutesAfter=30", self.appVersion];
-    XCTAssertEqualObjects(goodURLString, URL.absoluteString);
+    [self assertURL:URL equivalentToString:goodURLString];
 }
 
 - (void)testEncodingOfStopIDsWithSlashesAndSpaces {
     NSURL *URL = [self URLForRequestWithStopID:@"Foo/Bar Baz" minutesBefore:5 minutesAfter:30];
     NSString *goodURLString = [NSString stringWithFormat:@"http://api.tampa.onebusaway.org/api/api/where/arrivals-and-departures-for-stop/Foo%%2FBar%%20Baz.json?key=org.onebusaway.iphone&app_uid=test&app_ver=%@&version=2&minutesBefore=5&minutesAfter=30", self.appVersion];
-    XCTAssertEqualObjects(goodURLString, URL.absoluteString);
+    [self assertURL:URL equivalentToString:goodURLString];
 }
 
-
-- (NSURL*)URLForRequestWithStopID:(NSString*)stopID minutesBefore:(NSInteger)minutesBefore minutesAfter:(NSInteger)minutesAfter {
+- (NSURL*)URLForRequestWithStopID:(NSString*)stopID
+                    minutesBefore:(NSInteger)minutesBefore
+                     minutesAfter:(NSInteger)minutesAfter {
     NSURLRequest *originalRequest = [self.promisedModelService buildURLRequestForStopArrivalsAndDeparturesWithID:stopID minutesBefore:minutesBefore minutesAfter:minutesAfter];
     return originalRequest.URL;
 }
 
 - (NSString*)appVersion {
     return [OBAApplication sharedApplication].formattedAppBuild;
+}
+
+#pragma mark - Assertion Helpers
+
+/**
+ * Assert the given URL is semantically identical to the given good URL string,
+ * such that there's no difference except for the order of the URLs' query items.
+ *
+ * @param URL the actual URL obtained.
+ * @param goodURLString the expected URL string to get.
+ */
+- (void)assertURL:(NSURL*)URL equivalentToString:(NSString*)goodURLString {
+    NSURLComponents *URLComponents = [NSURLComponents componentsWithURL:URL resolvingAgainstBaseURL:YES];
+    NSURLComponents *goodURLComponents = [NSURLComponents componentsWithString:goodURLString];
+    [self normalizeURLComponents:URLComponents];
+    [self normalizeURLComponents:goodURLComponents];
+    XCTAssertEqualObjects(URLComponents, goodURLComponents);
+}
+
+/**
+ * Sort the query items of the given URLComponents by their names.
+ *
+ * @param URLComponents the NSURLComponents to normalize.
+ */
+- (void)normalizeURLComponents:(NSURLComponents*)URLComponents {
+    NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES];
+    URLComponents.queryItems = [URLComponents.queryItems sortedArrayUsingDescriptors:@[sortDescriptor]];
 }
 
 @end
